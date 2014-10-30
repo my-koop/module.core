0c021dd1ed5be4f914651587f9f727e0bd1e660c
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-10-30 02:52:01 -0400

    Imported core components from the website service.

---
+++ components/DashboardWrapper.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+
+var BSCol = require("react-bootstrap/Col");
+
+var MKPageWrapper = require("./PageWrapper");
+var MKSideBar = require("./SideBar");
+
+var DashboardWrapper = React.createClass({
+  render: function() {
+    return (
+      <MKPageWrapper dashboard>
+        {/* Side bar. */}
+        <BSCol md={2} style={{padding: 0}}>
+          <MKSideBar />
+        </BSCol>
+
+        {/* Main dashboard content. */}
+        <BSCol md={10}>
+          {this.props.activeRouteHandler()}
+        </BSCol>
+      </MKPageWrapper>
+    );
+  }
+});
+
+module.exports = DashboardWrapper;
--- components/index.ts
+++
@@ -1,7 +0,0 @@
-// List the components you want to expose at the root of your module, they,
-// in turn, can use other components that are not exposed as children if they so
-// desire.
-export var PublicWrapper     = require("./PublicWrapper");
-export var Homepage          = require("./Homepage");
-export var ParentPlaceHolder = require("./ParentPlaceHolder");
-export var PlaceHolder       = require("./PlaceHolder");
