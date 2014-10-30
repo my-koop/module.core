66b00bbd3c81707de367081c72cd06b91d6475fb
ad <stepanian.adam@live.com>
2014-10-30 15:39:22 -0400

    transfered form input factory from module.user
    Conflicts:
    	components/FormInputFactory.jsx
    	components/index.js
    	components/index.ts

--- components/FormInputFactory.jsx
+++ components/FormInputFactory.jsx
@@ -2,84 +2,101 @@ var React = require("react");
 var PropTypes = React.PropTypes;
 var BSInput = require("react-bootstrap/Input");
 
-
-
- generateSelectInput = function(properties,key)
- {
-   options = properties.options.map(function(option,key){
-     return (
-       <option key={key} name={option.name}>
-         {option.value}
-       </option>
-      );
-    });
-    return (
-      <BSInput type="select" key={key} label={properties.label} name={properties.name}>
+function generateSelectInput(properties, key) {
+ options = properties.options.map(function(option, key) {
+   return (
+     <option key={key} name={option.name}>
+       {option.value}
+     </option>
+    );
+  });
+  return (
+    <div>
+      <BSInput
+        type="select"
+        key={key}
+        label={properties.label}
+        name={properties.name}
+      >
         {options}
       </BSInput>
-    );       
- }
-    
- generateTextInput = function(properties,key){
+    </div>
+  );
+}
+
+function generateTextInput(properties, key) {
+  return (
+    <BSInput
+      type        ="text"
+      key         ={key}
+      label       ={properties.label}
+      name        ={properties.name}
+      defaultValue={properties.defaultValue}
+      placeholder ={properties.placeholder}
+      value       ={properties.value}
+    />
+  );
+}
+
+function generatePasswordInput(properties, key) {
    return (
-     <BSInput 
-       type        ="text"
-       key         ={key}
-       label       ={properties.label} 
-       name        ={properties.name} 
-       defaultValue={properties.defaultValue} 
-       placeholder ={properties.placeholder} 
-       value       ={properties.value} />
+     <BSInput
+        type       ="password"
+        key        ={key}
+        label      ={properties.label}
+        name       ={properties.name}
+        placeholder={properties.placeholder}
+      />
    );
  }
 
- generateRadioInputGroup = function(properties,key){
-    var name = properties.name;
-    var group = properties.values.map(function(radio,key){
-      return (
-        <BSInput type="radio" key={key} label={radio.label} name={name} value={radio.value} />            
-      );
-    });
+function generateRadioInputGroup(properties, key) {
+  var name = properties.name;
+  var group = properties.values.map(function(radio, key) {
     return (
-      <div className="form-inline" key={key}>
-        {group}
-      </div>
+      <BSInput type="radio" key={key} label={radio.label} name={name} value={radio.value} />
     );
- }
+  });
 
- generateCheckboxInput = function(properties,key){
-    return (
-       <div>
-        <BSInput 
-          key     ={key}
-          type    ="checkbox" 
-          label   ={properties.label} 
-          name    ={properties.name} 
-          selected={properties.isChecked} 
-          onChange={properties.function} />
-      </div>
-    );
- }
-     
-  InputFactoryMethod = function(type,properties,key){
-    var element;
-      switch(type) {
-        case "select": element = generateSelectInput(properties,key);
-                        break;
-        case "text":   element = generateTextInput(properties,key);
-                        break;
-        case "radio":  element = generateRadioInputGroup(properties,key);
-                        break;
-        case "checkbox" : element = generateCheckboxInput(properties,key);
-                          break;
-        default: break;
-      };
-      return (
-        <div>
-            {element}
-        </div>
-      );
-  }
+  return (
+    <div className="form-inline" key={key}>
+      {group}
+    </div>
+  );
+}
 
+function generateCheckboxInput(properties, key) {
+  return (
+    <BSInput
+      key     ={key}
+      type    ="checkbox"
+      label   ={properties.label}
+      name    ={properties.name}
+      selected={properties.isChecked}
+      onChange={properties.function} />
+  );
+}
 
-module.exports = InputFactoryMethod ;
+function InputFactoryMethod(type, properties, key) {
+  switch(type) {
+    case "select":
+      return generateSelectInput(properties,key);
+
+    case "text":
+      return generateTextInput(properties,key);
+
+    case "radio":
+      return generateRadioInputGroup(properties,key);
+
+    case "checkbox":
+      return generateCheckboxInput(properties,key);
+
+    case "password":
+      return generatePasswordInput(properties,key);
+
+    default:
+      throw new Error("Input type not found.");
+  };
+}
+
+module.exports = InputFactoryMethod;
