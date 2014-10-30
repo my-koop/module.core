3c631d52765d9f03657eb5a129ea9a79ebc75a3a
Cellule <mike.ferris@hotmail.com>
2014-10-30 02:59:32 -0400

    Wrapper for the MenuLink with React-Router

---
+++ components/MenuItemLink.jsx
@@ -0,0 +1,41 @@
+var React = require("react");
+var Router = require("react-router");
+var BSMenuItem = require("react-bootstrap/MenuItem");
+
+var MenuItemLink = React.createClass({
+  mixins: [Router.ActiveState],
+
+  propTypes: {
+    to: React.PropTypes.string
+  },
+
+  getInitialState: function () {
+    return {
+      isActive: false
+    };
+  },
+
+  updateActiveState: function() {
+    this.setState({
+      //isActive: false
+      //FIXME: For now "/" is considered active when in "/users".
+      isActive: MenuItemLink.isActive(this.props.to, this.props.params, this.props.query)
+    });
+  },
+
+  render: function() {
+    return this.transferPropsTo(
+      <BSMenuItem
+        onSelect={this.handleClick}
+      >
+        {this.props.children}
+      </BSMenuItem>
+    );
+  },
+
+  handleClick: function() {
+    Router.transitionTo(this.props.to, this.props.params, this.props.query);
+  }
+});
+
+module.exports = MenuItemLink;
