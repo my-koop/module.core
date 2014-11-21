320a01358f940303d321159c1ffe655532fe3087
Cellule <mike.ferris@hotmail.com>
2014-11-21 14:34:08 -0500

    CR Fix

--- components/Feedbacki18nMixin.jsx
+++ components/Feedbacki18nMixin.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+var React = require("react");
 
 var MKAlert = require("./Alert");
 var __ = require("language").__;
@@ -12,9 +12,12 @@ module.exports = {
   },
 
   setFeedback: function(feedbackList, style) {
+    if(!_.isArray(feedbackList)) {
+      feedbackList = [feedbackList]
+    }
     this.setState({
       __feedback: {
-        i18n: _.toArray(feedbackList),
+        i18n: feedbackList,
         style: style
       }
     });
@@ -26,14 +29,22 @@ module.exports = {
     });
   },
 
-  getFeedback: function(alertProps) {
+  renderFeedback: function(alertProps) {
     if(_.isEmpty(this.state.__feedback)) {
       return null;
     }
 
-    var feedback = _.map(this.state.__feedback.i18n, function(f, i) {
-      return <div key={i}>{__(f.key, _.omit(f, "key"))}</div>;
-    });
+    var feedback = null;
+    var length = this.state.__feedback.i18n.length;
+    if(length === 1) {
+      var f = this.state.__feedback.i18n[0];
+      feedback = __(f.key, _.omit(f, "key"));
+    } else if(length > 0) {
+      feedback = _.map(this.state.__feedback.i18n, function(f, i) {
+        return <li key={i}>{__(f.key, _.omit(f, "key"))}</li>;
+      });
+      feedback = <ul>{feedback}</ul>;
+    }
 
     return (
       <MKAlert
