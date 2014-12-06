5e6c1c367fca78733075aa7a76e5e394d4b271cb
Cellule <mike.ferris@hotmail.com>
2014-12-05 19:32:10 -0500

    provided methods and controllers to interact with a note table

--- typings/mykoop-core/interfaces.d.ts
+++ typings/mykoop-core/interfaces.d.ts
@@ -18,4 +18,38 @@ declare module mkcore {
       (err?: Error): void
     }
   }
+
+  export interface Note {
+    date: Date;
+    message: string;
+    author: string;
+  }
+
+  module GetNotes {
+    export interface Params {
+      table: string;
+      id: number;
+    }
+    export interface Result {
+      notes: Note[];
+    }
+    export interface Callback {
+      (err: Error, res?: Result): void
+    }
+  }
+
+  module NewNote {
+    export interface Params {
+      table: string;
+      targetId: number;
+      authorId: number;
+      message: string;
+    }
+    export interface Result {
+      notes: Note[];
+    }
+    export interface Callback {
+      (err: Error, res?: Result): void
+    }
+  }
 }
